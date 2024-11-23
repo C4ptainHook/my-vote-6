32f5e889e1adc4fff4e1f59faf3f3da729fd764b
CaptainHook <boykostudyingkpi@gmail.com>
2024-11-23 14:02:28 +0200

    Try to restore database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,15 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize('myvote-lab5', process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mysql',
-  port: 24582
-});
+const sequelize = new Sequelize(
+  "myvote-lab5",
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+    port: 21014,
+  }
+);
 
 module.exports = sequelize;
